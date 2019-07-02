ea25a32b722c920d98cedc60b16532c8dd70f1b3
Nakul Tyagi <31548192+NakulTyagi@users.noreply.github.com>
2019-07-02 15:36:28 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,41 +1,41 @@
 $(document).ready(function (){
-            $("#home").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.home").offset().top
-                }, 1200);
-            });
-             $("#about").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.about").offset().top
-                }, 1200);
-            });
-    
-            $("#port").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.about").offset().top
-                }, 1200);
-            });
-            $("#skills").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.skills").offset().top
-                }, 1200);
-            });
-             $("#exp").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.experience").offset().top
-                }, 1200);
-            });
-            $("#contact").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.contact").offset().top
-                }, 1200);
-            });
-    
-            $("#view").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("div.about").offset().top
-                }, 1200);
-            });
+//            $("#home").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.home").offset().top
+//                }, 1200);
+//            });
+//             $("#about").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.about").offset().top
+//                }, 1200);
+//            });
+//    
+//            $("#port").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.about").offset().top
+//                }, 1200);
+//            });
+//            $("#skills").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.skills").offset().top
+//                }, 1200);
+//            });
+//             $("#exp").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.experience").offset().top
+//                }, 1200);
+//            });
+//            $("#contact").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.contact").offset().top
+//                }, 1200);
+//            });
+//    
+//            $("#view").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("div.about").offset().top
+//                }, 1200);
+//            });
     
             $(".hom").click(function(){
                 $("#view").stop().slideToggle(120);
